6b2d080eb3ab2afc93997f03ae1f6c4d6c57516c
shirakurak <surakura@hotmail.co.jp>
2023-08-16 17:00:54 +0900

    up

--- tmp/sample.fsx
+++ tmp/sample.fsx
@@ -2,6 +2,8 @@
 
 printfn "start"
 
+// --------------------
+
 // ラムダ式の定義
 type Lambda =
   | Variable of char
@@ -19,11 +21,29 @@ let E_2 = Abstraction('v', E_1) // λv.(λu.u)
 let a = Variable('a')
 let E = Application(E_1, a) // (λu.u)a
 
+// --------------------
+
 // 深さ
-let rec calLambdaDepth (e : Lambda) =
+let rec calLambdaDepth e =
   match e with
   | Variable(u) -> 1
   | Abstraction(u, e1) -> 1 + calLambdaDepth(e1)
   | Application(e1, e2) -> calLambdaDepth(e1) + calLambdaDepth(e2)
+
+// --------------------
+
+// 出現している変数
+let rec allvar e =
+  match e with
+  | Variable(u) -> [ u ]
+  | Abstraction(u, e1) -> u :: allvar(e1)
+  | Application(e1, e2) -> allvar(e1) @ allvar(e2)
+
+// 出現している自由変数
+
+// 出現している束縛変数
+
+// λへの翻訳
+// CalcFormulaみたいに
 
 printfn "goal"
