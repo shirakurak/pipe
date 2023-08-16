468b2ad989cf9c0b3dac81864017ad9dba0d010f
shirakurak <surakura@hotmail.co.jp>
2023-08-16 16:13:35 +0900

    up

--- tmp/sample.fsx
+++ tmp/sample.fsx
@@ -2,16 +2,23 @@
 
 printfn "start"
 
-type Variable = char
+// type Variable = char
 
 // ラムダ式の定義
 type Lambda =
-  | Variable of Variable
-  | Abstraction of Head : Variable * Body : Lambda
+  | Variable of char
+  | Abstraction of Head : char * Body : Lambda
   | Application of Function : Lambda * Argument : Lambda
 
-// Abstractionを生成する関数
-let abstracter(v, E) =
-  Abstraction(v, E)
+// 例1（変数u）
+let u = Variable('u') // u : Lambda
+
+// 例2（ラムダ抽象）
+let E_1 = Abstraction('u', u) // λu.u
+let E_2 = Abstraction('v', E_1) // λv.(λu.u)
+
+// 例3（関数適用）
+let a = Variable('a')
+let E = Application(E_1, a) // (λu.u)a
 
 printfn "goal"
