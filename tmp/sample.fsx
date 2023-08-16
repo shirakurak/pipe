0ed967393fbffd8460814ebdc72028ed4602c1f2
shirakurak <surakura@hotmail.co.jp>
2023-08-16 15:47:07 +0900

    abstracter

--- tmp/sample.fsx
+++ tmp/sample.fsx
@@ -4,9 +4,14 @@ printfn "start"
 
 type Variable = char
 
+// ラムダ式の定義
 type Lambda =
   | Variable of Variable
   | Abstraction of Head : Variable * Body : Lambda
   | Application of Function : Lambda * Argument : Lambda
+
+// Abstractionを生成する関数
+let abstracter(v, E) =
+  Abstraction(v, E)
 
 printfn "goal"
